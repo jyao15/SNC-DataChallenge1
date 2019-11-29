f2ce22e73e3f772e172d2cd6a56fefb0645dac78
no-more-github <yyljkbgthg2@163.com>
2019-11-29 04:45:16 -0500

    force full coverage

--- task2.py
+++ task2.py
@@ -20,18 +20,13 @@ def BFS(graph, seed):
     return list(influenced_dict.keys())
 
 
-def calculate_objective(graph, region_total_count, region_selected_count, influenced_nodes, softmin=True):
+def calculate_objective(region_total_count, region_selected_count):
     region_selected_count = region_selected_count.copy()
-    for node in influenced_nodes:
-        region_selected_count[graph.nodes[node]['region']] += 1
     proportions = []
     for region, size in region_total_count.items():
         selected_count = region_selected_count[region]
         proportions.append(selected_count / size)
-    if softmin:
-        objective = np.sum(np.sqrt(np.asarray(proportions))) ** 2
-    else:
-        objective = np.asarray(proportions).min()
+    objective = np.asarray(proportions).min()
     return objective
 
 
@@ -48,14 +43,22 @@ for node in G.nodes:
     region_total_count[G.nodes[node]['region']] += 1
 region_selected_count = defaultdict(int)
 
+full_coverage = False
+covered_regions = {}
 for iter in range(25):
     max_index, max_objective = -1, -1
-    for seed in candidates:
+    if full_coverage:
+        valid_candidates = candidates
+    else:
+        valid_candidates = []
+        for node in candidates:
+            if G.nodes[node]['region'] not in covered_regions:
+                valid_candidates.append(node)
+    for seed in valid_candidates:
         objectives = []
         for newG_index, newG in zip(range(len(newGs)), newGs):
             influenced_nodes = BFS(newG, seed)
-            objective = calculate_objective(G, region_total_count, region_selected_count, influenced_nodes)
-            objectives.append(objective)
+            objectives.append(len(influenced_nodes))
         ave_objective = np.asarray(objectives).mean()
         if ave_objective > max_objective:
             max_index, max_objective = seed, ave_objective
@@ -66,12 +69,20 @@ for iter in range(25):
     for newG_index, newG in zip(range(len(newGs)), newGs):
         influenced_nodes = BFS(newG, max_index)
         newG.remove_nodes_from(influenced_nodes)
+    covered_regions[G.nodes[max_index]['region']] = None
+    if len(covered_regions) == 13:
+        full_coverage = True
 
 print('Seed set:\n', selected)
 print('Average number of people influenced (including seeds):')
-influenced_nums = []
+influenced_nums, objectives = [], []
 for newG_index, newG in zip(range(len(newGs)), newGs):
-    influenced_nums.append(len(G.nodes) - len(newG.nodes))
+    influenced_nodes = set(G.nodes).difference(set(newG.nodes))
+    influenced_nums.append(len(influenced_nodes))
+    region_selected_count.clear()
+    for node in influenced_nodes:
+        region_selected_count[G.nodes[node]['region']] += 1
+    objectives.append(calculate_objective(region_total_count, region_selected_count))
 print(np.asarray(influenced_nums).mean())
 print('Average minimal of people influenced (including seeds):')
-print(calculate_objective(G, region_total_count, region_selected_count, [], softmin=False))
+print(np.asarray(objectives).mean())
