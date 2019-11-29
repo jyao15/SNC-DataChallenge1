48749f9eeb2a01ee5ece32c9bb1c14fba839f750
no-more-github <yyljkbgthg2@163.com>
2019-11-29 01:09:04 -0500

    slow task 1 partial result added, fast task 1 running

--- task1.py
+++ task1.py
@@ -4,11 +4,10 @@ import numpy as np
 import queue
 
 
-def BFS(graph, seeds):
-    influenced_dict = {seed: None for seed in seeds}
+def BFS(graph, seed):
+    influenced_dict = {seed: None}
     BFS_queue = queue.Queue(500)
-    for seed in seeds:
-        BFS_queue.put(seed)
+    BFS_queue.put(seed)
     while not BFS_queue.empty():
         first = BFS_queue.get()
         for node in graph.successors(first):
@@ -29,19 +28,25 @@ selected = list()
 
 for iter in range(25):
     max_index, max_influenced = -1, -1
-    for candidate in candidates:
-        seeds = selected.copy()
-        seeds.append(candidate)
+    for seed in candidates:
         influenced_nums = []
         for newG_index, newG in zip(range(len(newGs)), newGs):
-            influenced_nodes = BFS(newG, seeds)
-            influenced_nums.append(len(influenced_nodes) - len(seeds))
+            if seed in newG.nodes:
+                influenced_nodes = BFS(newG, seed)
+                influenced_nums.append(len(influenced_nodes) - 1)
+            else:
+                influenced_nums.append(0)
         ave_influenced = np.asarray(influenced_nums).mean()
         if ave_influenced > max_influenced:
-            max_index, max_influenced = candidate, ave_influenced
+            max_index, max_influenced = seed, ave_influenced
     candidates.remove(max_index)
     selected.append(max_index)
     print('Greedy selected seed:', max_index)
+
+    for newG_index, newG in zip(range(len(newGs)), newGs):
+        if max_index in newG.nodes:
+            influenced_nodes = BFS(newG, max_index)
+            newG.remove_nodes_from(influenced_nodes)
 
 print('Seed set:\n', selected)
 print('Average number of people influenced:\n', max_influenced)
