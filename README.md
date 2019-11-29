664c0b39f2971275bf11aa34f0f19ff35d85233c
no-more-github <yyljkbgthg2@163.com>
2019-11-29 04:13:48 -0500

    softmin failed

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# SNC-DataChallenge1" 
+"# SNC-DataChallenge1"
+
+First run pre_simulation.py to generate the 500 graphs.
+
+Then run task1.py to perform task1 and print all results.
+
+Or run task2.py to perform task2 and print all results.
--- task2.py
+++ task2.py
@@ -20,6 +20,21 @@ def BFS(graph, seed):
     return list(influenced_dict.keys())
 
 
+def calculate_objective(graph, region_total_count, region_selected_count, influenced_nodes, softmin=True):
+    region_selected_count = region_selected_count.copy()
+    for node in influenced_nodes:
+        region_selected_count[graph.nodes[node]['region']] += 1
+    proportions = []
+    for region, size in region_total_count.items():
+        selected_count = region_selected_count[region]
+        proportions.append(selected_count / size)
+    if softmin:
+        objective = np.sum(np.sqrt(np.asarray(proportions))) ** 2
+    else:
+        objective = np.asarray(proportions).min()
+    return objective
+
+
 with open('graph.pickle', 'rb') as input_graph_file:
     G = pickle.load(input_graph_file)
 
@@ -28,17 +43,22 @@ with open('simulation.pickle', 'rb') as input_simulation_file:
 
 candidates = list(G.nodes)
 selected = list()
+region_total_count = defaultdict(int)
+for node in G.nodes:
+    region_total_count[G.nodes[node]['region']] += 1
+region_selected_count = defaultdict(int)
 
 for iter in range(25):
-    max_index, max_influenced = -1, -1
+    max_index, max_objective = -1, -1
     for seed in candidates:
-        influenced_nums = []
+        objectives = []
         for newG_index, newG in zip(range(len(newGs)), newGs):
             influenced_nodes = BFS(newG, seed)
-            influenced_nums.append(len(influenced_nodes))
-        ave_influenced = np.asarray(influenced_nums).mean()
-        if ave_influenced > max_influenced:
-            max_index, max_influenced = seed, ave_influenced
+            objective = calculate_objective(G, region_total_count, region_selected_count, influenced_nodes)
+            objectives.append(objective)
+        ave_objective = np.asarray(objectives).mean()
+        if ave_objective > max_objective:
+            max_index, max_objective = seed, ave_objective
     candidates.remove(max_index)
     selected.append(max_index)
     print('Greedy selected seed', iter + 1, ':', max_index)
@@ -53,16 +73,5 @@ influenced_nums = []
 for newG_index, newG in zip(range(len(newGs)), newGs):
     influenced_nums.append(len(G.nodes) - len(newG.nodes))
 print(np.asarray(influenced_nums).mean())
-
-region_total_count = defaultdict(int)
-for node in G.nodes:
-    region_total_count[G.nodes[node]['region']] += 1
-region_selected_count = defaultdict(int)
-for newG_index, newG in zip(range(len(newGs)), newGs):
-    influenced_nodes = set(G.nodes).difference(set(newG.nodes))
-    for node in influenced_nodes:
-        region_selected_count[G.nodes[node]['region']] += 1 / len(newGs)
-for region, total_count in region_total_count.items():
-    selected_count = region_selected_count[region]
-    print(region, '\nsize:', total_count, '   influenced (including seeds):', selected_count,
-          '   proportion:', selected_count / total_count)
+print('Average minimal of people influenced (including seeds):')
+print(calculate_objective(G, region_total_count, region_selected_count, [], softmin=False))
